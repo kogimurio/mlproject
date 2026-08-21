0f371486c04ff3b060ea2b12341a073cf7c5ed4f
Kenneth <kogimurio@gmail.com>
2026-08-21 07:23:00 +0300

    statistics

--- linearRegression.py
+++ linearRegression.py
@@ -15,7 +15,26 @@ chicago_taxi_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-dat
 # updates daataframe to use specific columns.
 training_df = chicago_taxi_dataframe.loc[:, ('TRIP_MILES', 'TRIP_SECONDS', 'FARE', 'COMPANY', 'PAYMENT_TYPE', 'TIP_RATE')]
 training_df.head(200)
-print(training_df.describe(include='all'))
+# print(training_df.describe(include='all'))
+
+# What is the maximum fare?
+max_fare = training_df['FARE'].max()
+print(f"What is the maximum fare?  Answer: ${max_fare:.2f}")
+
+# What is the mean distance across all trips?
+mean_distance = training_df['TRIP_MILES'].mean()
+print(f"What is the mean distance across all trips? Answer: {mean_distance:.4f}")
+
+# How many cabs companies are in the dataset?
+num_unique_companies = len(training_df['COMPANY'].unique())
+print(f"How many cabs companies are in the dataset? Answer: {num_unique_companies}")
 
 
+# What is the most frequent payment type?
+most_freq_payment_type = training_df['PAYMENT_TYPE'].value_counts().idxmax()
+print(f"What is the most frequent payment type? Answer: {most_freq_payment_type}")
+
+# Are any feature missing data?
+missing_values = training_df.isnull().sum().sum()
+print(f"Are any feature missing data? Answer:", "No" if missing_values == 0 else "Yes")
 
